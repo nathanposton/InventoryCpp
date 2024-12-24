479ac12bcd1dccba1e294aec2d955eb2c18a7a0e
Nathan Poston <67882689+nathanposton@users.noreply.github.com>
2024-12-24 11:16:41 -0700

    Inventory Tutorial #8:
    + files added from editor for:
        slot,
        drag and drop,
        tooltip,
        drag operation,
        custom player controller (for easier item interaction).
    
    PROGRESS TIMESTAMP:
    4:54
    LINK:
    https://www.youtube.com/watch?v=KxUsSq8Sfv8&list=PLitYOdh3OOzhJBCRqYZEGBzJYemA-SH8Z&index=9

---
+++ Source/InventoryCpp/Public/Player/InventoryTutPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InventoryTutPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYCPP_API AInventoryTutPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
