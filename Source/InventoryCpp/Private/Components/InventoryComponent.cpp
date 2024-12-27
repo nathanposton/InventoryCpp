f584a262090d4807c6c2268205d6febdeb64080a
Nathan Poston <67882689+nathanposton@users.noreply.github.com>
2024-12-27 12:30:51 -0700

    Inventory Tutorial #10:
    debugging handlestackableitems
    
    TODO:
    - pretty up the inventory UI ( slots, etc.)
    - items hover mid-air after dropping (to fix this, I can just enable physics)
    - fix dragging within menu triggering drop action
    
    PROGRESS TIMESTAMP:
    39:23
    LINK:
    https://www.youtube.com/watch?v=KxUsSq8Sfv8&list=PLitYOdh3OOzhJBCRqYZEGBzJYemA-SH8Z&index=11

--- Source/InventoryCpp/Private/Components/InventoryComponent.cpp
+++ Source/InventoryCpp/Private/Components/InventoryComponent.cpp
@@ -162,7 +162,108 @@ FItemAddResult UInventoryComponent::HandleNonStackableItems(
 int32 UInventoryComponent::HandleStackableItems(UItemBase* ItemIn,
                                                 int32 RequestedAddAmount)
 {
-	return 0;
+	// FIXME:
+	// error: when attempting to pick up 15 potions of weight 2.
+	// max stack size: 5
+	// weight limit: 50
+	// inventory contents before pickup: 2 stacks of 5, 1 stack of 1
+	// inventory weight before pickup: 22/50
+	// slot count before pickup: 3/20
+	//
+	// after attempting pickup of 15 potions:
+	// pickup disappears
+	// one stack of 5 potions is gained
+	// 10 weight is added to inventory.
+	// one slot is added to inventory.
+	// this is wrong.
+	//
+	// expected outcome:
+	// pickup remains, with one potion left
+	// the stack of 1 potion is filled to 5
+	// 2 more stacks of 5 are added.
+	// weight ends at 50/50
+	// slots end at 5/20
+
+	// check if requested amount is valid
+	if (RequestedAddAmount <= 0 || FMath::IsNearlyZero(
+		ItemIn->GetItemStackWeight()))
+	{
+		return 0;
+	}
+
+	int32 AmountToDistribute = RequestedAddAmount;
+	UItemBase* ExistingItemStack = FindNextPartialStack(ItemIn);
+
+	// add to any partial stacks first
+	while (ExistingItemStack)
+	{
+		const int32 AmountToMakeFullStack = CalculateNumberForFullStack(
+			ExistingItemStack, AmountToDistribute);
+		const int32 WeightLimitAddAmount = CalculateWeightAddAmount(
+			ExistingItemStack, AmountToMakeFullStack);
+
+		// add if there is weight capacity
+		if (WeightLimitAddAmount > 0)
+		{
+			ExistingItemStack->SetQuantity(ExistingItemStack->Quantity +
+				WeightLimitAddAmount);
+			InventoryTotalWeight += (WeightLimitAddAmount * ExistingItemStack->
+				GetItemSingleWeight());
+
+			AmountToDistribute -= WeightLimitAddAmount;
+			ItemIn->SetQuantity(AmountToDistribute);
+
+			// if weight limit reached/exceeded, halt the loop
+			//TODO: Refine this logic since going over weight capacity should not be possible
+			if (InventoryTotalWeight >= InventoryWeightCapacity)
+			{
+				OnInventoryUpdated.Broadcast();
+				return RequestedAddAmount - AmountToDistribute;
+			}
+		}
+		// if no more weight capacity, halt the loop
+		else if (WeightLimitAddAmount <= 0)
+		{
+			if (AmountToDistribute != RequestedAddAmount)
+			{
+				OnInventoryUpdated.Broadcast();
+				return RequestedAddAmount - AmountToDistribute;
+			}
+
+			return 0;
+		}
+
+		// all of the input item was distributed across stacks
+		if (AmountToDistribute <= 0)
+		{
+			OnInventoryUpdated.Broadcast();
+			return RequestedAddAmount;
+		}
+
+		// current partial stack filled, still more to distribute. Get next partial stack
+		ExistingItemStack = FindNextPartialStack(ItemIn);
+	}
+
+	// No more partial stacks found, check if a new stack can be added
+	while (AmountToDistribute > 0 && InventoryContents.Num() < InventorySlotsCapacity)
+	{
+		const int32 WeightLimitAddAmount = CalculateWeightAddAmount(ItemIn, AmountToDistribute);
+
+		if (WeightLimitAddAmount > 0)
+		{
+			const int32 AmountToAdd = FMath::Min(WeightLimitAddAmount, ItemIn->NumericData.MaxStackSize);
+			AddNewItem(ItemIn->CreateItemCopy(), AmountToAdd);
+			AmountToDistribute -= AmountToAdd;
+			ItemIn->SetQuantity(AmountToDistribute);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	OnInventoryUpdated.Broadcast();
+	return RequestedAddAmount - AmountToDistribute;
 }
 
 FItemAddResult UInventoryComponent::HandleAddItem(UItemBase* InputItem)
